5f5b0ce36ddd99d66abdccb17239de02a5b6760c
phiedulxp <phiedulxp@gmail.com>
2018-01-05 17:02:45 +0800

    update

---
+++ spider/master_korea.py
@@ -0,0 +1,50 @@
+import re
+import json
+import fire
+import time
+from tqdm import tqdm
+from datetime import datetime,timedelta
+from collections import Counter
+from Config import get_spider_config
+_,db,r = get_spider_config()
+
+
+users = [i['tweet']['user']['screen_name'] for i in db.dataset_korea_m_1.find({},{"tweet.user.screen_name":1})]
+freq_users = [i[0] for i in Counter(users).most_common() if i[1]>=5]
+            
+def get_query_str(loc,triggers,target):
+	# start = (now - time_delta).strftime("%Y-%m-%d %H:%M:%S")
+	# now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+	return '('+loc+')' + ' '+'('+' OR '.join(triggers)+')'+' '+'('+target+')'
+
+def get_task():
+	locs=["North Korea"]
+	triggers=["test","launch","fire"]
+	targets = ["messile","satellite","rocket","nuclear"]
+	now = datetime.now()
+	WAIT_TIME = 15
+	while True:
+		for loc in locs:
+			for target in targets:
+				q = get_query_str(loc,triggers,target)
+					message = {'q':q,'f':['&f=news','','&f=tweets'],'num':-1,
+					"sinceTimeStamp":(now - timedelta(minutes=WAIT_TIME)).strftime("%Y-%m-%d %H:%M:%S"),
+					"untilTimeStamp":now.strftime("%Y-%m-%d %H:%M:%S")
+					}
+					print(message)
+					r.rpush("task:korea",json.dumps(message))
+		for user in freq_users:
+			message = {'q':'from:'+user,'f':'&f=tweets','num':-1,
+					"sinceTimeStamp":(now - timedelta(minutes=WAIT_TIME)).strftime("%Y-%m-%d %H:%M:%S"),
+					"untilTimeStamp":now.strftime("%Y-%m-%d %H:%M:%S")
+			}
+			print(message)
+			r.rpush('task:korea',json.dumps(message))
+		time_gone = (datetime.now()-now).seconds
+		if time_gone < 60*WAIT_TIME:
+			time.sleep(60*WAIT_TIME-time_gone)
+			now = datetime.now()
+		else:
+			now = now+timedelta(minutes=WAIT_TIME)
+if __name__ == '__main__':
+	get_task()
---
+++ spider/worker_korea.py
@@ -0,0 +1,62 @@
+import sys
+if sys.version_info[0] < 3:
+	import got
+else:
+	import got3 as got
+    
+import time
+import json
+    
+import multiprocessing
+from multiprocessing import Pool
+from datetime import datetime
+
+from Config import get_spider_config
+_,db,r = get_spider_config()
+
+def advance_search_korea(q,f,num,s,u):
+
+	collection = db.korea
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch(q).setTweetType(f).setSinceTimeStamp(s).setUntilTimeStamp(u).setMaxTweets(num)
+	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+	for tweet in tweets:
+		if collection.find_one({'_id':tweet['id']}) == None:
+			collection.insert_one({'_id':tweet['id'],'tweet':tweet,'f':f,'q':q})
+
+def run_korea_task(message_data):
+	try:
+		q = message_data['q']
+		num = message_data['num']
+		sinceTimeStamp = datetime.strptime(message_data['sinceTimeStamp'],"%Y-%m-%d %H:%M:%S")
+		untilTimeStamp = datetime.strptime(message_data['untilTimeStamp'],"%Y-%m-%d %H:%M:%S")
+		if type(message_data['f']) != list:
+			advance_search_korea(q,message_data['f'],num,sinceTimeStamp,untilTimeStamp)
+		else:
+			pool = Pool(processes=multiprocessing.cpu_count())
+			[pool.apply(advance_search_korea,(q,f,num,sinceTimeStamp,untilTimeStamp)) for f in message_data['f']]
+			pool.close()
+			pool.join()
+	except Exception,e:
+		raise Exception(e.message)
+  
+if __name__ == '__main__':
+	print 'craw_worker start!'
+	while True:
+		queue = r.lpop('task:korea')
+		queue_len = r.llen('task:korea')
+		if queue:
+			print 'craw_worker process!'
+			try:
+				run_korea_task(json.loads(queue))
+				db.korea_log.insert_one({'message':json.loads(queue),'status':1})
+			except Exception,e:
+				db.korea_log.insert_one({'message':json.loads(queue),'status':0,'error':e.message})
+			if queue_len == 0:
+				message = {"is_last":True}
+				r.rpush('task:classify',json.dumps(message))
+			#else:
+				#message = {"is_last":False}
+				#r.rpush('task:classify',json.dumps(message))	
+
+		time.sleep(1)
+		print 'craw_worker wait!'
